366843ab896822ad7545e6a52244a478add71d85
danielspiker <danielmnascimento@gmail.com>
2021-09-03 13:23:35 -0300

    refatoração

--- prog-imperativa/aula12/bonus.js
+++ prog-imperativa/aula12/bonus.js
@@ -1,11 +1,11 @@
 function digitalHouse(a, b) {
   for (let i = 1; i <= 100; i++) {
-    if ((a % i == 0 && b % i == 0) || (i % a == 0 && i % b == 0)) {
+    if ((a % i == 0 || i % a == 0) && (b % i == 0 || i % b == 0)) {
       console.log('digital house')
-    } else if (b % i == 0 || i % b == 0) {
-      console.log('house')
     } else if (a % i == 0 || i % a == 0) {
       console.log('digital')
+    } else if (b % i == 0 || i % b == 0) {
+      console.log('house')
     } else {
       console.log(i)
     }
