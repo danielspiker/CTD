a2e4e310f13333e5398833ee32eadecc680e9b34
danielspiker <danielmnascimento@gmail.com>
2021-09-29 16:26:33 -0300

    alteração no checkpoint

--- prog-imperativa/checkpoint3/curso.js
+++ prog-imperativa/checkpoint3/curso.js
@@ -3,8 +3,6 @@ let Aluno = require('./Aluno.js')
 let aluno1 = new Aluno('Cebolinha', 1, [6, 8, 7, 6])
 let aluno2 = new Aluno('Magali', 2, [10, 7, 8, 9])
 
-/* let alunos = [aluno1, aluno2] */
-
 let curso = {
   nomeCurso: 'Programação',
   notaAprovação: 7,
@@ -43,16 +41,17 @@ let curso = {
 
 let aluno3 = new Aluno('Cascão', 0, [7.5, 8.2, 7.4, 8])
 
-curso.adicionaAluno(aluno3)
-
 curso.adicionaAluno(aluno1)
 curso.adicionaAluno(aluno2)
+curso.adicionaAluno(aluno3)
 console.log(curso.estudantes)
 aluno3.acrescentaFalta()
 aluno3.acrescentaFalta()
 aluno3.acrescentaFalta()
-
 console.log(curso.estudantes)
 console.log(curso.alunoAprovado(aluno1))
 console.log(curso.alunoAprovado(aluno3))
 console.log(curso.aprovados())
+console.log(aluno1.calcularMedia())
+console.log(aluno2.calcularMedia())
+console.log(aluno3.calcularMedia())
